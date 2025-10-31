eadcd63f49db424032804514c40ccf6feec74d69
Spleefies <cutecatty32@gmail.com>
2025-10-31 22:14:38 +0800

    fizzing candy in the food science lab

--- bus.js
+++ bus.js
@@ -1,4 +1,4 @@
-import maplibregl from 'maplibre-gl'
+import maplibregl, { LngLat } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
 const map = new maplibregl.Map({
@@ -7,12 +7,11 @@ const map = new maplibregl.Map({
   center: [103.818108, 1.3431684],
   zoom: 11
 })
-
+let stopsData
 map.on('load', async () => {
   try {
     const res = await fetch('https://data.busrouter.sg/v1/stops.min.geojson')
-    const stopsData = await res.json()
-
+    stopsData = await res.json()
     map.addSource('bus-stops', {
       type: 'geojson',
       data: stopsData
@@ -38,46 +37,92 @@ map.on('load', async () => {
       },
       filter: ['==', '$type', 'Point']
     })
-
+    
     map.on('click', 'bus-stops-layer', (e) => {
       const feature = e.features[0]
       const coords = feature.geometry.coordinates.slice()
       const props = feature.properties || {}
       
       const html = `
-          <strong>${props.name || 'Unknown'}</strong> <br>
-          <strong>Stop ID:</strong> ${props.number || 'N/A'}<br>
-          <button onclick="console.log(${props.number})">Lock in</button>
-        `
-      
-      new maplibregl.Popup()
-      .setLngLat(coords)
-      .setHTML(html)
-      .addTo(map)
+      <strong>${props.name || 'Unknown'}</strong> <br>
+      <strong>Stop ID:</strong> <span id="stopNo">${props.number || 'N/A'}</span><br>
+      <button id="lockIn">Lock in</button>
+      `
+      new maplibregl.Popup().setLngLat(coords).setHTML(html).addTo(map)
+      updateButton()
     })
-
+    
     map.on('mouseenter', 'bus-stops-layer', () => map.getCanvas().style.cursor = 'pointer')
     map.on('mouseleave', 'bus-stops-layer', () => map.getCanvas().style.cursor = '')
-  } catch (err) {
-    console.error('Error loading stops:', err)
-  }
+  } catch (err) {console.error('Error loading stops:', err)}
 })
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://xowfiaodrlbghhoobmbm.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhvd2ZpYW9kcmxiZ2hob29ibWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3OTI4MDQsImV4cCI6MjA3NjM2ODgwNH0.eOze-pW1rD7TN9-fR7FTA4QMcG3Mcip7RIZmAInQ7z0"
 const supabase = createClient(supabaseUrl, supabaseKey)
-let imgNo
+let imgNo, imgName
 async function getImgUrl() {
   const { data, error } = await supabase.storage.from('Images').list()
   if (error) {
     console.error(error)
   } else {
     imgName = data[Math.floor(Math.random()*data.length)].name
-    console.log(imgName)
     imgNo = imgName.slice(0,3)
+    console.log(imgNo)
     return supabase.storage.from('Images').getPublicUrl(imgName).data.publicUrl
   }
 }
 const mainimg = document.querySelector("#mainimg")
 getImgUrl().then(url => {mainimg.src = url})
+async function loadStops() {
+  const { data, error } = await supabase.from('stops').select()
+  
+  if (error) console.error(error) 
+    else {
+    let ans = data.find(i => i.img_no == imgNo)
+    console.log(ans.stop_no,guess)
+    let guessLocation = new LngLat(...getLngLatById(guess))
+    let ansLocation = new LngLat(...getLngLatById(ans.stop_no))
+    console.log(guessLocation.distanceTo(ansLocation))
+    map.addSource('route', {
+      'type': 'geojson',
+      'data': {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': [
+            getLngLatById(ans.stop_no),
+            getLngLatById(guess),
+          ]
+        }
+      }
+    }
+    )
+  map.addLayer({
+    'id': 'route',
+    'type': 'line',
+    'source': 'route',
+    'layout': {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    'paint': {
+      'line-color': '#248623',
+      'line-width': 8
+    }
+  });
+  map.flyTo({center: getLngLatById(ans.stop_no), zoom: 16, speed:0.67})
+}
+}
+function updateButton(){
+  lockInButton = document.querySelector("#lockIn")
+  lockInButton.addEventListener("click", loadStops)
+  stopNoSpan = document.querySelector("#stopNo")
+  guess = stopNoSpan.textContent
+}
+function getLngLatById(id) {
+  const feature = stopsData.features.find(f => f.id === id)
+  return feature?.geometry.coordinates
+}
